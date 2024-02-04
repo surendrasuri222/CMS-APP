eb0bd5f4702527a4bc11812b9b682bf71c1a41a3
Sourav6434 <souravranjanmallick6434@gmail.com>
2024-02-04 09:44:10 +0530

    Added pages Component to app.js

---
+++ src/Components/pages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UsersPage() {
+    return (
+        <div>
+            <h1>Pages will be diplay here</h1>
+        </div>
+    )
+        
+}
+
+export default UsersPage;
