60e889957e0534bca9be10f7b3f83ee45d34c255
Sourav6434 <souravranjanmallick6434@gmail.com>
2024-02-06 12:14:35 +0530

    Added the User Component

---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const User = () => {
+    return (
+        <>
+        <h1>User Content will be shown here</h1>
+        <h1>User Content Display here</h1>
+        </>
+    )
+}
+
+export default User;
