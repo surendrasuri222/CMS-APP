b90e4d258e6bf5b8acff754d43a7edb9c99c3afa
Sagar Kundu <sagarkundukv2@gmail.com>
2024-02-08 15:51:36 +0530

    Update: Aligned Headers

--- ClientSide/src/components/Users.jsx
+++ ClientSide/src/components/Users.jsx
@@ -22,84 +22,82 @@ const Users = () => {
         <>
             <section>
                 <div class="container shadow-lg p-3 mb-5 bg-white rounded p-5">
-                    <div class="row">
-                        <div className="row">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h1 className="book-header"><i className="glyphicon glyphicon-file"></i> Users</h1>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="btn-group actions" role="group" aria-label="...">
 
-
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <h1 class="page-header"><i class="glyphicon glyphicon-user"></i> Users</h1>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="btn-group actions" role="group" aria-label="...">
-                                            {/* New - Modal Button */}
-                                            <UserModal />
-                                        </div>
+                                        {/* New Modal For Creating Books */}
+                                        <UserModal />
                                     </div>
                                 </div>
-
-                                <ol class="breadcrumb">
-                                    <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-                                    <li class="active">Users</li>
-                                </ol>
-
-                                <table id="sort-table" class="table table-striped tablesorter">
-                                    <thead>
-                                        <tr>
-
-                                            <th>Full Name <i class="glyphicon glyphicon-chevron-down"></i></th>
-                                            <th>Email <i class="glyphicon glyphicon-chevron-down"></i></th>
-                                            <th>Group <i class="glyphicon glyphicon-chevron-down"></i></th>
-                                            <th>Action <i class="glyphicon glyphicon-chevron-down"></i></th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-
-
-
-
-                                        {
-                                            users.map((user) => {
-                                                return (
-                                                    <tr className='p-2'>
-                                                        <td><NavLink to="/users/user">{user.fullname}</NavLink></td>
-                                                        <td>{user.email}</td>
-                                                        <td>{user.group}</td>
-                                                        <td><button className='btn btn-default bi bi-trash3-fill p-2 '></button></td>                                                    </tr>)
-
-                                            })
-                                        }
-                                    </tbody>
-                                </table>
-                                <div className="d-flex justify-content-center h-2 ">
-                                    <nav aria-label="Page navigation">
-                                        <ul class="pagination">
-                                            <li>
-                                                <a href="#" aria-label="Previous">
-                                                    <span aria-hidden="true">&laquo;</span>
-                                                </a>
-                                            </li>
-                                            <li><a href="#">1</a></li>
-                                            <li><a href="#">2</a></li>
-                                            <li><a href="#">3</a></li>
-                                            <li><a href="#">4</a></li>
-                                            <li><a href="#">5</a></li>
-                                            <li>
-                                                <a href="#" aria-label="Next">
-                                                    <span aria-hidden="true">&raquo;</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                </div>
-
                             </div>
+
+                            <ol class="breadcrumb">
+                                <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+                                <li class="active">Users</li>
+                            </ol>
+
+                            <table id="sort-table" class="table table-striped tablesorter table-hover">
+                                <thead>
+                                    <tr>
+
+                                        <th>Full Name <i class="glyphicon glyphicon-chevron-down"></i></th>
+                                        <th>Email <i class="glyphicon glyphicon-chevron-down"></i></th>
+                                        <th>Group <i class="glyphicon glyphicon-chevron-down"></i></th>
+                                        <th>Action <i class="glyphicon glyphicon-chevron-down"></i></th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+
+
+
+
+                                    {
+                                        users.map((user) => {
+                                            return (
+                                                <tr className='p-2'>
+                                                    <td><NavLink to="/users/user">{user.fullname}</NavLink></td>
+                                                    <td>{user.email}</td>
+                                                    <td>{user.group}</td>
+                                                    <td><button className='btn btn-default bi bi-trash3-fill p-2 '></button></td>                                                    </tr>)
+
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                            <div className="d-flex justify-content-center h-2 ">
+                                <nav aria-label="Page navigation">
+                                    <ul class="pagination">
+                                        <li>
+                                            <a href="#" aria-label="Previous">
+                                                <span aria-hidden="true">&laquo;</span>
+                                            </a>
+                                        </li>
+                                        <li><a href="#">1</a></li>
+                                        <li><a href="#">2</a></li>
+                                        <li><a href="#">3</a></li>
+                                        <li><a href="#">4</a></li>
+                                        <li><a href="#">5</a></li>
+                                        <li>
+                                            <a href="#" aria-label="Next">
+                                                <span aria-hidden="true">&raquo;</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+
                         </div>
                     </div>
                 </div>
-            </section>
+
+            </section >
 
             <footer>
                 <p>Copyright 2017, All Rights Reserved</p>
