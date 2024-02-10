09dd2f960a8c4ad65e8b52e5bb668a9375c4aa61
Sagar Kundu <sagarkundukv2@gmail.com>
2024-02-10 09:19:33 +0530

    Update: Responsive Navbar

--- ClientSide/src/components/Navbar.jsx
+++ ClientSide/src/components/Navbar.jsx
@@ -1,63 +1,81 @@
 import { NavLink } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom'
+import 'remixicon/fonts/remixicon.css'
 
+import React, { useState } from 'react';
+import '../css/navbar.scss' // Importing the CSS file
 
 const Navbar = () => {
-    const navigate = useNavigate()
+    const [showMenu, setShowMenu] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
 
-    const handleLogOut = () => {
-        localStorage.clear()
-        navigate("/")
-    }
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const toggleSearch = () => {
+        setShowSearch(!showSearch);
+    };
+
+
+    // const toggleLogin = () => {
+    //     setShowLogin(!showLogin);
+    // };
+
     return (
-        <div>
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button
-                            type="button"
-                            className="navbar-toggle collapsed"
-                            data-toggle="collapse"
-                            data-target="#bs-example-navbar-collapse-1"
-                        >
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
 
-                    </div>
+        <nav className={`nav container-fluid d-flex w-100 mb-5 `}>
+            <h4 className='white-text flex-row'><NavLink to='/dashboard' className="nav__logo">DCX CMS</NavLink></h4>
 
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav flex flex-row  d-flex justify-content-center">
-                            <li><a class="navbar-brand" href="#">DCX CMS</a></li>
+            <div className={`nav__menu ${showMenu ? 'show-menu' : ''}`} style={{ zIndex: 1 }}>
+                <ul className="nav__list my-auto" >
+                    <li className="nav__item my-auto" style={{ fontSize: '14px' }}>
+                        <NavLink to="/dashboard">Dashboard</NavLink>
+                    </li>
+                    <li className="nav__item my-auto" style={{ fontSize: '14px' }}>
+                        <NavLink to='/pages'>Pages</NavLink>
+                    </li>
+                    <li className="nav__item my-auto" style={{ fontSize: '14px' }}>
+                        <NavLink to='/categories'>Categories</NavLink>
+                    </li>
+                    <li className="nav__item my-auto" style={{ fontSize: '14px' }}>
+                        <NavLink to='/users'>Users</NavLink>
+                    </li>
+                    <li className="nav__item my-auto">
 
-                            <li><NavLink to="/dashboard" activeClassName="active">Dashboard</NavLink></li>
-                            <li><NavLink to="/pages" exact activeClassName="active">Pages</NavLink></li>
+                    </li>
 
-                            <li><NavLink to="/categories" exact activeClassName="active">Categories</NavLink></li>
-                            <li><NavLink to="/users" exact activeClassName="active">Users</NavLink></li>
-                        </ul>
-                        <form className="navbar-form navbar-left  d-flex justify-content-center" role="search">
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Search" />
-                            </div>
-                            <button type="submit" className="btn btn-default">Submit</button>
-                        </form>
-                        <ul className="nav navbar-nav navbar-right flex flex-row  d-flex justify-content-center">
-                            <li><NavLink to="/userprofile" activeClassName="active">My Profile</NavLink></li>
-                            <li><button className='btn btn-primary' onClick={handleLogOut}>Logout</button></li>
-                        </ul>
+                </ul>
 
-                    </div>
+                <div className="nav__close my-auto" onClick={toggleMenu}>
+                    <i className="ri-close-line"></i>
                 </div>
-            </nav>
+            </div>
 
-        </div>
-    )
+            <div className="nav__actions d-flex align-items-center  my-auto">
+                <i className="ri-search-line nav__search  my-auto" onClick={toggleSearch} style={{ fontSize: '18px' }}></i>
+                {
+                    showSearch && (
+                        <div className="search-popup border border-white  my-auto">
+                            <input type="text" placeholder="Search by Category" />
+                        </div>
+                    )
+                }
+                <NavLink to='/userprofile' className="ri-user-line nav__login  my-auto" style={{ fontSize: '18px' }} ></NavLink>
+                <div className="nav__toggle" onClick={toggleMenu}>
+                    <i className="ri-menu-line"></i>
+                </div>
 
-}
+                <div>
+                    <button className='btn btn primary btn-sm  btn-outline-light'>Logout</button>
+                </div>
 
+
+            </div>
+
+        </nav>
+    );
+};
 
 export default Navbar;
+
